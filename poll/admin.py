d9b4271d89b2574984d26f607157c46dd0dee63b
laxezice <61070048@kmitl.ac.th>
2020-05-03 16:06:14 +0700

    edited2

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'desc']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'question']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin) 
