4eea3763fca49e3e177269626bdae7308bc4fc7d
laxezice <61070048@kmitl.ac.th>
2020-05-03 15:59:41 +0700

    edited starter code

--- poll/models.py
+++ poll/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Choice(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-       return self.name
-
 class Question(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
 
     def __str__(self):
-       return self.name   
+       return self.name 
+
+class Choice(models.Model):
+    name = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.name  
